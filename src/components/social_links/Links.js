0c79579f95cc6cdfc180fe3c141d0e003b2f2f03
giftedNathan <eje.nathaniel@gmail.com>
2022-11-01 13:38:03 +0100

    Updasted social icons; removed whatapp icon and added github icon

--- src/components/social_links/Links.js
+++ src/components/social_links/Links.js
@@ -1,25 +1,28 @@
 import './links.css'
-import { FaTwitter, FaFacebook, FaLinkedin, FaYoutube, FaWhatsapp } from 'react-icons/fa'
+import { FaTwitter, FaFacebook, FaLinkedin, FaYoutube, FaWhatsapp, FaGithub } from 'react-icons/fa'
 
 
 export const Links = () => {
     return (
         <div className="social-links">
+            <a href="https://www.github.com/giftednathan" target="_blank" rel="noreferrer">
+                <FaGithub className="fab fa-whatsapp" />
+            </a>
+            <a href="https://www.linkedin.com/in/nathaniel-akenyi-eje" target="_blank" rel="noreferrer">
+                <FaLinkedin className="fab fa-linkedin" />
+            </a>
             <a href="https://www.twitter.com/eje_nathaniel" target="_blank" rel="noreferrer">
                 <FaTwitter className="fab fa-twitter" />
             </a>
             <a href="https://www.facebook.com/eje.nathaniel" target="_blank" rel="noreferrer">
                 <FaFacebook className="fab fa-facebook" />
             </a>
-            <a href="https://www.linkedin.com/in/eje-nathaniel-82047790/" target="_blank" rel="noreferrer">
-                <FaLinkedin className="fab fa-linkedin" />
-            </a>
-            <a href="https://www.youtube.com/channel/UCXuGb8ghyaTLc0deDKdDs3Q" target="_blank" rel="noreferrer">
+            {/* <a href="https://www.youtube.com/channel/UCXuGb8ghyaTLc0deDKdDs3Q" target="_blank" rel="noreferrer">
                 <FaYoutube className="fab fa-youtube" />
-            </a>
-            <a href="/" target="_blank" rel="noreferrer">
-                <FaWhatsapp className="fab fa-whatsapp" />
-            </a>
+            </a> */}
+            {/* <a href="https://www.github.com/giftednathan" target="_blank" rel="noreferrer">
+                <FaGithub className="fab fa-whatsapp" />
+            </a> */}
         </div>
     )
 }
