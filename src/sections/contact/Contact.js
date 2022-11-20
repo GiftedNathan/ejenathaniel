fc399d9f3eee5c0e9fb82cc08d5f3995fdbd9e2e
Eje <eje.nathaniel@gmail.com>
2022-11-20 15:22:12 +0100

    Updated contact.js; added form validation using react hook form library

--- src/sections/contact/Contact.js
+++ src/sections/contact/Contact.js
@@ -4,9 +4,11 @@ import './contact.css'
 
 import React, { useRef } from 'react';
 import emailjs from 'emailjs-com';
+import { useForm } from "react-hook-form";
+
 
 const Contact = ({title, subtitle, note}) => {
-
+    const { register, formState: { errors }, handleSubmit } = useForm();
     const form = useRef();
 
     const sendEmail = (e) => {
@@ -16,6 +18,7 @@ const Contact = ({title, subtitle, note}) => {
         .then((result) => {
             // console.log(result.text);
             alert("Thank you for contacting Eje Nathaniel, He will respond sooner than you expect.");
+            form.reset()
         }, (error) => {
             // console.log(error.text);
             alert("Opps, something went wrong. Check internet connection and try again.");
@@ -25,14 +28,52 @@ const Contact = ({title, subtitle, note}) => {
 
     return (
         <div id="contact" className="contact-section">
+            
             <SectionHeader title={title} subtitle={subtitle} note={note} />
-            <form className="contact-form" ref={form} onSubmit={sendEmail}>
-                <input type="text" className="name" name="user_name" placeholder="name" />
-                <input type="email" className="email" name="user_email" placeholder="email" />
-                <input type="text" className="subject" name="user_subject" placeholder="subject" />
-                <textarea name="message" id="message" cols="30" rows="5" className="message" placeholder="subject"></textarea>
+            
+            <form className="contact-form" ref={form} onSubmit={handleSubmit(sendEmail)}>
+                {/* <input type="text" className="name" name="user_name" placeholder="name" /> */}
+                <input 
+                    type="text" className="name" name="user_name" placeholder="Full Name" id="name" 
+                    {...register("name", { required: true, maxLength: 65, minLength: 3 })} 
+                    aria-invalid={errors.name ? "true" : "false"} 
+                />
+
+                {errors.name && errors.name.type === "required" && <p className='error'>Full name is required</p>}
+                {errors.name && errors.name.type === "maxLength" && <p className='error'>Max length of 65 characters exceeded</p> }
+                {errors.name && errors.name.type === "minLength" && <p className='error'>Name must be more than 3 characters</p> }
+                {/* {errors.fullName?.type === 'required' && <p className='error' role="alert">Full name is required *</p>} */}
                 
-                <Button name={'Submit'} />
+                {/* <input type="email" className="email" name="user_email" placeholder="email" /> */}
+                <input 
+                    type="email" className="email" name="user_email" placeholder="email"
+                    {...register("mail", { required: "Email Address is required *" })} 
+                    aria-invalid={errors.mail ? "true" : "false"} 
+                />
+                {errors.mail && <p className='error' role="alert">{errors.mail?.message}</p>}
+
+
+                {/* <input type="text" className="subject" name="user_subject" placeholder="subject" /> */}
+                <input 
+                    type="text" className="subject" name="user_subject" placeholder="Subject"
+                    {...register("subject", { required: true })} 
+                    aria-invalid={errors.subject ? "true" : "false"} 
+                />
+                {errors.subject?.type === 'required' && <p className='error' role="alert">Subject is required *</p>}
+
+
+                {/* <textarea name="message" id="message" cols="30" rows="5" className="message" placeholder="Your message"></textarea> */}
+                <textarea
+                    name="message" id="message" cols="30" rows="5" className="message" placeholder="Your message"
+                    {...register("message", { required: true, minLength: 3 })} 
+                    aria-invalid={errors.message ? "true" : "false"} 
+                >
+                </textarea>
+                {errors.message?.type === 'required' && <p className='error' role="alert">Message field is required *</p>}
+                {errors.message && errors.message.type === "minLength" && <p className='error'>Name must be more than 3 characters</p> }
+                
+                <Button type="submit" name={'Submit'} />
+
             </form>
         </div>
     )
