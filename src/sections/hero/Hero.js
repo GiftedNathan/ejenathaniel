45f9779d26532ed2a65f5fa69024bbd73fdda046
Eje <eje.nathaniel@gmail.com>
2023-03-17 18:07:35 +0100

    Updated projects.js; added recent wordpress projects

--- src/sections/hero/Hero.js
+++ src/sections/hero/Hero.js
@@ -8,71 +8,72 @@ import Logo from '../../logo-nathan/logo-nath-white.png'
 
 
 
-const Hero = ({name, designation, greeting}) => {
-    // const learnMoreIcon = <i className="fa fa-arrow-down" aria-hidden="true"></i>
-    const learnMoreIcon = < FaArrowCircleDown className="learn-more-icon" />
-    
-    // Create reference to store the DOM element containing the animation
-	  const el = React.useRef(null);
+const Hero = ({ name, designation, greeting }) => {
+  // const learnMoreIcon = <i className="fa fa-arrow-down" aria-hidden="true"></i>
+  const learnMoreIcon = < FaArrowCircleDown className="learn-more-icon" />
 
-    // Create reference to store the Typed instance itself
-    const typed = React.useRef(null);
-  
-    React.useEffect(() => {
-      const options = {
-          strings: [
-          'Frontend Developer',
-          'Graphics Designer',
-          'Web Designer',
-          'Frontend Developer',
-          'Good with ReactJs',
-          'Good with SCSS',
-          'Good with Wordpress',
-          'Clean codes',
-        ],
-        typeSpeed: 80,
-        backSpeed: 80,
-        loop: true,
-        // loopCount: Infinity,
-        showCursor: true,
-        cursorChar: ']',
-        autoInsertCss: true,
-      };
-      
-      // elRef refers to the <span> rendered below
-      typed.current = new Typed(el.current, options);
-      
-      return () => {
-        // Make sure to destroy Typed instance during cleanup
-        // to prevent memory leaks
-        typed.current.destroy();
-      }
-    }, [])
-  
-    
-    return (
-        <section className="hero-section bg-gradient-close">
-            <div className="hero-section-content">
-                <p>{greeting}</p>
-                <h1 className="">Work with <span  >{'<'}  {name} {'/>'} </span> </h1>
-                <img src={Logo}  className="hero-image" alt="hero" />
-                <div className="designation">
-                  <span className="typed-cursor">[</span> 
-                  <h3 ref={el}>{designation}</h3>
-                </div>
-            </div>
+  // Create reference to store the DOM element containing the animation
+  const el = React.useRef(null);
 
-            <a href="#about"><Button name={'Learn more'} icon={learnMoreIcon} /></a>
+  // Create reference to store the Typed instance itself
+  const typed = React.useRef(null);
 
-            <Links />
-        </section>
-    )
+  React.useEffect(() => {
+    const options = {
+      strings: [
+        'Frontend Developer',
+        'WordPress Developer',
+        'Graphics Designer',
+        'Frontend Developer',
+        'Web Designer',
+        'Good with ReactJs',
+        'Good with SCSS',
+        'Lover of WordPress',
+        'Clean codes',
+      ],
+      typeSpeed: 80,
+      backSpeed: 80,
+      loop: true,
+      // loopCount: Infinity,
+      showCursor: true,
+      cursorChar: ']',
+      autoInsertCss: true,
+    };
+
+    // elRef refers to the <span> rendered below
+    typed.current = new Typed(el.current, options);
+
+    return () => {
+      // Make sure to destroy Typed instance during cleanup
+      // to prevent memory leaks
+      typed.current.destroy();
+    }
+  }, [])
+
+
+  return (
+    <section className="hero-section bg-gradient-close">
+      <div className="hero-section-content">
+        <p>{greeting}</p>
+        <h1 className="">Work with <span  >{'<'}  {name} {'/>'} </span> </h1>
+        <img src={Logo} className="hero-image" alt="hero" />
+        <div className="designation">
+          <span className="typed-cursor">[</span>
+          <h3 ref={el}>{designation}</h3>
+        </div>
+      </div>
+
+      <a href="#about"><Button name={'Learn more'} icon={learnMoreIcon} /></a>
+
+      <Links />
+    </section>
+  )
 }
 
 Hero.defaultProps = {
-    name: 'NathanEje',
-    designation: 'Frontend Developer',
-    greeting: 'hello world'
+  name: 'NathanEje',
+  designation: 'Frontend Developer',
+  greeting: 'hello world'
 }
 
 
--- src/sections/projects/Projects.js
+++ src/sections/projects/Projects.js
@@ -2,23 +2,78 @@ import Button from '../../components/button/Button'
 import { SectionHeader } from '../../components/section-header/SectionHeader'
 import './projects.css'
 
-const Projects = ({title, subtitle, note}) => {
+import studentTaxHelpImage from '../../assets/images/studenttaxhelp.PNG'
+import rulebornImage from '../../assets/images/ruleborn.PNG'
+import isoxImage from '../../assets/images/isox.PNG'
+import emcaImage from '../../assets/images/emca.PNG'
+
+const Projects = ({ title, subtitle, note }) => {
     return (
         <div id="portfolio" className="projects-section">
             <SectionHeader title={title} subtitle={subtitle} note={note} />
             <div className="project-container">
                 <div className="project">
+                    <img src={studentTaxHelpImage} class="project-image" alt="student tax help" />
+                    <h3 className="project-title">
+                        Student Taxt Help
+                    </h3>
+                    <p className="project-text">
+                        Filling of student taxes became easy with this wordpress website
+                    </p>
+                    <a href='https://studenttaxhelp.ca/' target='_blank' rel="noreferrer">
+                        <Button name={'Check it out'} />
+                    </a>
+                </div>
+                <div className="project">
+                    <img src={rulebornImage} class="project-image" alt="student tax help" />
+                    <h3 className="project-title">
+                        Ruleborn intergrated
+                    </h3>
+                    <p className="project-text">
+                        Ruleborn intergrated  wordpress website
+                    </p>
+                    <a href='https://rulebornintegrated.com/' target='_blank' rel="noreferrer">
+                        <Button name={'Check it out'} />
+                    </a>
+                </div>
+                <div className="project">
+                    <img src={isoxImage} class="project-image" alt="student tax help" />
+                    <h3 className="project-title">
+                        Isox tech limited
+                    </h3>
+                    <p className="project-text">
+                        Isox tech limited wordpress website
+                    </p>
+                    <a href='https://isoxltd.com/' target='_blank' rel="noreferrer">
+                        <Button name={'Check it out'} />
+                    </a>
+                </div>
+                <div className="project">
+                    <img src={emcaImage} class="project-image" alt="student tax help" />
+                    <h3 className="project-title">
+                        EMCA
+                    </h3>
+                    <p className="project-text">
+                        EMCA wordpress website
+                    </p>
+                    <a href='https://www.emcaaccounting.ca/' target='_blank' rel="noreferrer">
+                        <Button name={'Check it out'} />
+                    </a>
+                </div>
+
+
+                <div className="project">
                     <h3 className="project-title">
                         Easyrecipe
                     </h3>
                     <p className="project-text">
-                        A simple recipe app that fetches a list of all 
-                        recipes from external API and provide dynamic 
+                        A simple recipe app that fetches a list of all
+                        recipes from external API and provide dynamic
                         search functionality.
                     </p>
                     <p className="project-technology">
-                        <span>Concepts Used:</span> React router dom, React Hooks, components, props, useState(), 
-                        useEffect() with managing dependencies, handling HTML input in React. 
+                        <span>Concepts Used:</span> React router dom, React Hooks, components, props, useState(),
+                        useEffect() with managing dependencies, handling HTML input in React.
                         Fetch API, JS Promises, and callback. React Styled Components; styling with flexbox and grid.
                     </p>
                     <a href='https://easyrecipee.netlify.app/' target='_blank' rel="noreferrer">
@@ -27,36 +82,36 @@ const Projects = ({title, subtitle, note}) => {
                 </div>
                 <div className="project">
                     <h3 className="project-title">
-                        My Portfolio 
+                        My Portfolio
                     </h3>
                     <p className="project-text">
-                        This is fully responsive portfolio website using react with multiple sections. 
+                        This is fully responsive portfolio website using react with multiple sections.
                         It has various animations and support mobile, desktop, and tablet screen sizes.
                     </p>
                     <p className="project-technology">
-                        <span>Concepts Used:</span> React Icons, React Hooks, components, 
+                        <span>Concepts Used:</span> React Icons, React Hooks, components,
                         props, useState(), react-router. TypedJS for some animations.
-                         CSS styling with flexbox, variables, animations and many more. 
+                        CSS styling with flexbox, variables, animations and many more.
                     </p>
                     <a href="https://ejenathaniel.netlify.app/" rel="noreferrer">
-                        <Button name={'Check it out'}/>
+                        <Button name={'Check it out'} />
                     </a>
                 </div>
                 <div className="project">
                     <h3 className="project-title">
-                        Translator 
+                        Translator
                     </h3>
                     <p className="project-text">
-                        A simple language translator with text-to-voice and copy functionality. 
+                        A simple language translator with text-to-voice and copy functionality.
                     </p>
                     <p className="project-technology">
                         <span>Concepts Used:</span> The Memory API was used to build the tranlation functionality.
                         The web speech API in JavaScript was used to tranplate the text to speech.
                         Tha Asynchronous clipboard API was used to build the copy funtionality.
-                        CSS styling with flexbox and many more. 
+                        CSS styling with flexbox and many more.
                     </p>
                     <a href="https://giftednathan.github.io/translator" target='_blank' rel="noreferrer">
-                        <Button name={'Check it out'}/>
+                        <Button name={'Check it out'} />
                     </a>
                 </div>
                 <div className="project">
@@ -64,13 +119,13 @@ const Projects = ({title, subtitle, note}) => {
                         Animated Weather icons
                     </h3>
                     <p className="project-text">
-                        An animated weather icons for the various weather conditions. 
+                        An animated weather icons for the various weather conditions.
                         It is built with HTML AND CSS.
                     </p>
-                    
+
                     <p className="project-technology">
-                        <span>Concepts Used:</span> HTML and CSS styling with flexbox, variables, 
-                        keyfram animations and many more. 
+                        <span>Concepts Used:</span> HTML and CSS styling with flexbox, variables,
+                        keyfram animations and many more.
                     </p>
                     <a href='https://giftednathan.github.io/animated-weather-icons' target='_blank' rel="noreferrer">
                         <Button name={'Check it out'} />
@@ -81,22 +136,22 @@ const Projects = ({title, subtitle, note}) => {
                         Many More
                     </h3>
                     <p className="project-text">
-                        Trust me, i have more project 
-                        that currently aren't showcased here. Hit the button 
+                        Trust me, i have more project
+                        that currently aren't showcased here. Hit the button
                         below to view my github repositories.
                     </p>
                     <p className="project-technology">
-                        <span>Different projects built with:</span> ReactJs (React router dom, React Hooks, components, props, useState(), 
+                        <span>Different projects built with:</span> ReactJs (React router dom, React Hooks, components, props, useState(),
                         useEffect() React Styled components) Javascrips, SCSS, APIs
-                         and many more. 
+                        and many more.
                     </p>
                     <a href='https://www.github.com/giftednathan' target='_blank' rel="noreferrer">
                         <Button name={'More on GitHub'} />
                     </a>
                 </div>
             </div>
-        
-        
+
+
         </div>
     )
 }
